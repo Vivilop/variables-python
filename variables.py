0ef5ef31e3860d6c0fe2d0e7a2785878af933ea9
Viviana Lopez <vivilopezlo77@gmail.com>
2023-10-08 19:56:41 -0500

    se define cada variable primitiva y se imprime en  la terminal de salida

---
+++ variables.py
@@ -0,0 +1,18 @@
+# tipo de variable entera (init)
+numero_entero = 40
+print (numero_entero)
+# tipo de variable flotante (float)
+numero_float = 3.15
+print ( numero_float)
+# variable tipo cadena (str)
+cadena = "Hola, mundo !"
+print (cadena)
+# variable tipo boolenao True o False 
+es_verdadero = True
+print(es_verdadero)
+es_falso =False
+print (es_falso)
+
+
+
+
