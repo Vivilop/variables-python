0d3148a0d90b84b10eb4f6915d3c085765067553
Viviana Lopez <vivilopezlo77@gmail.com>
2023-10-08 21:29:13 -0500

    se concatena las cadenas de variables primitivas

--- variables.py
+++ variables.py
@@ -13,6 +13,12 @@ print(es_verdadero)
 es_falso =False
 print (es_falso)
 
-
+# concatenacion de cadenas con variables primitivas 
+nombre = "Viviana"
+edad = 40
+altura = 1.75 
+#usanso el operador + para concatenar cadenas y variable 
+mensaje = 'Hola, mi nombre es ' + nombre + ', tengo ' +  str (edad) +  'años y mido' +  str (altura) + ' metros. '
+print (mensaje)
 
 
